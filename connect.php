a94d4807f5d8482491bcd546d2dbaaf43ad7de4a
Danny <imac@iMac.broadband>
2020-06-09 16:30:12 +0100

    first commit

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+
+	$hostname="localhost"; //local server name default localhost
+	$username="root";  //mysql username default is root.
+	$password="";       //blank if no password is set for mysql.
+	$database="/Users/imac/Documents/Html/LDSounds/Users.db";  //database name which you created
+	$connect= "mssql $hostname,$username,$password";
+	if(! $con)
+	{
+	die('Connection Failed');
+	}
+
+	mysqli_select_db($database,$con);
+	?>
